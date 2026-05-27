75282b593b7c384811bfcd53bcf80496f178057c
CHENGJUHO <chengjuho@naver.com>
2026-05-28 01:46:56 +0900

    feat: 백준 문제 풀이 추가 (1000·1001)

---
+++ baekjoon_python/1000.py
@@ -0,0 +1,13 @@
+# 1
+# import sys
+# input = sys.stdin.readline
+
+# a, b = map(int, input().split())
+# print(a + b)
+
+
+print(sum(map(int, input().split())))
+
+# 3
+# import sys
+# print(sum(map(int, sys.stdin.read().split())))
